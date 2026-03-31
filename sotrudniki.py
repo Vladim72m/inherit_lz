ec09db1ea8f6c8d0c6af7123a957a68515a2a226
Vladim72m <vladim72dx@tutamail.com>
2026-03-31 20:53:20 +0300

    Update sotrudniki.py

--- sotrudniki.py
+++ sotrudniki.py
@@ -3,23 +3,23 @@ class Employee:
         self.name = name
         self.position = position
         self.salary = salary
-    def info(self):
+    def info(self):        # Выводит основную информацию о сотруднике
         print('Имя:', self.name)
         print('Должность:', self.position)
         print('Тарифная ставка:', self.salary)
 
-    def calc_salary(self, worked_days):
+    def calc_salary(self, worked_days):       # Считает зарплату
         money = self.position*self.salary*worked_days   
         return money
     
 class Manager(Employee):
     def __init__(self, name, position, salary, department, num_employees):
-        super().__init__(name, position, salary)
+        super().__init__(name, position, salary)    # Вызывает конструктор родительского класса и заполняет его параметры
         self.department = department
         self.num_employees = num_employees
 
     def info(self):
-        print("*"*10,"Вся информация о сотруднике","*"*10)
+        print("Вся информация о сотруднике")
         super().info()
         print('Отдел, в котором работает человек', self.department)
         print('Количество сотрудников в отделе', self.num_employees)
